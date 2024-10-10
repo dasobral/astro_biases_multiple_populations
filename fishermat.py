2c2721d46a71f905d2a33c56699c76a9d7ab1407
dasobral <daniel.sobralblanco@unige.ch>
2024-10-10 12:27:22 +0200

    added docstrings

--- fishermat.py
+++ fishermat.py
@@ -8,16 +8,30 @@ Created on Feb 2023
 """
 import numpy as np
 
-def cov_matrix_survey(d, z_bins, pixel_size = 4, small_off_diagonal = True, rtol = 1e-4, split=[50,50]):
-    
+def cov_matrix_survey(d, z_bins, pixel_size=4, small_off_diagonal=True, rtol=1e-4, split=[50, 50]):
+    """
+    Load the covariance matrix for a survey.
+
+    Parameters:
+    - d (array-like): The distance bins.
+    - z_bins (array-like): The redshift bins.
+    - pixel_size (float, optional): The pixel size. Default is 4.
+    - small_off_diagonal (bool, optional): Whether to remove small off-diagonal terms. Default is True.
+    - rtol (float, optional): The relative difference threshold for removing small off-diagonal terms. Default is 1e-4.
+    - split (list, optional): The population split percentages. Default is [50, 50].
+
+    Returns:
+    - CovMatrix (list of arrays): The covariance matrices for each redshift bin.
+    """
+
     d_min = d[0]
     d_max = d[-1]
-    
+
     print('\n ... Loading Covariance Matrices [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
     print(f'Population splitted in {split[0]}% BRIGHT - {split[1]}% FAINT \n')
-    
+
     CovMatrix = [np.loadtxt('Covariance/octupole/CovarianceMatrixOCT_at_z'+str(z)+'_d'+str(int(d_min))+'to160_msplit-'+str(split[0])+'B_'+str(split[1])+'F.txt') for z in z_bins]
-    
+
     if d_max != 160:
         for z,_ in enumerate(z_bins):
             ld = len(d)
@@ -26,7 +40,7 @@ def cov_matrix_survey(d, z_bins, pixel_size = 4, small_off_diagonal = True, rtol
             cols_to_remove = np.c_[ld:ld_max, (ld+ld_max):int(2*ld_max), (ld+int(2*ld_max)):int(3*ld_max), (ld+int(3*ld_max)):int(4*ld_max), (ld+int(4*ld_max)):int(5*ld_max), (ld+int(5*ld_max)):int(6*ld_max), (ld+int(6*ld_max)):int(7*ld_max), (ld+int(7*ld_max)):int(8*ld_max)]
             CovMatrix[z] = np.delete(CovMatrix[z], rows_to_remove, axis=0)
             CovMatrix[z] = np.delete(CovMatrix[z], cols_to_remove, axis=1)
-            
+
     if small_off_diagonal == False:
         print('\n ... Removing small off-diagonal terms vs diagonal terms ... \n')
         print(f'\n Relative difference = {rtol} \n')
@@ -41,20 +55,34 @@ def cov_matrix_survey(d, z_bins, pixel_size = 4, small_off_diagonal = True, rtol
                     else: 
                         Matrix[i,j] = Matrix[i,j]
             CovMatrix[n] = Matrix
-            
+
     print('\n ... Success! ... \n')
-    
+
     return CovMatrix
 
-def cov_matrix_joint(d, z_bins, small_off_diagonal = True, rtol = 1e-4, splits=[50,30], contributions = 'all'):
-    
+def cov_matrix_joint(d, z_bins, small_off_diagonal=True, rtol=1e-4, splits=[50, 30], contributions='all'):
+    """
+    Load the covariance matrix for a joint analysis of two population splits.
+
+    Parameters:
+    - d (array-like): The distance bins.
+    - z_bins (array-like): The redshift bins.
+    - small_off_diagonal (bool, optional): Whether to remove small off-diagonal terms. Default is True.
+    - rtol (float, optional): The relative difference threshold for removing small off-diagonal terms. Default is 1e-4.
+    - splits (list, optional): The bright population split percentages. Default is [50, 30].
+    - contributions (str, optional): The contributions to include in the covariance matrix. Default is 'all'.
+
+    Returns:
+    - CovMatrix (list of arrays): The covariance matrices for each redshift bin.
+    """
+
     d_min = d[0]
     d_max = d[-1]
-    
-    print('\n ... Loading Covariance Matrices (Joint-splittings Analysis) ['+str(contributions)+'] [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
-    
-    CovMatrix = [np.loadtxt('Covariance/octupole/CovarianceMatrixOCT_'+str(contributions)+'_'+str(splits[0])+'x'+str(splits[1])+'_at_z'+str(z)+'_d'+str(int(d_min))+'to160.txt') for z in z_bins]
-   
+
+    print('\n ... Loading Covariance Matrices (Joint-splittings Analysis) [' + str(contributions) + '] [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
+
+    CovMatrix = [np.loadtxt('Covariance/octupole/CovarianceMatrixOCT_' + str(contributions) + '_' + str(splits[0]) + 'x' + str(splits[1]) + '_at_z' + str(z) + '_d' + str(int(d_min)) + 'to160.txt') for z in z_bins]
+
     if small_off_diagonal == False:
         print('\n ... Removing small off-diagonal terms vs diagonal terms ... \n')
         print(f'\n Relative difference = {rtol} \n')
@@ -63,16 +91,30 @@ def cov_matrix_joint(d, z_bins, small_off_diagonal = True, rtol = 1e-4, splits=[
             r, c = np.shape(Matrix)
             for i in np.arange(r):
                 for j in np.arange(c):
-                    value = Matrix[i,j]/np.sqrt(Matrix[i,i] * Matrix[j,j])
+                    value = Matrix[i, j] / np.sqrt(Matrix[i, i] * Matrix[j, j])
                     if value <= rtol:
-                        Matrix[i,j] = 0.
-                    else: 
-                        Matrix[i,j] = Matrix[i,j]
+                        Matrix[i, j] = 0.
+                    else:
+                        Matrix[i, j] = Matrix[i, j]
             CovMatrix[n] = Matrix
-    
+
     return CovMatrix
 
-def inverse_cov_matrix_survey(d, z_bins, pixel_size = 4, small_off_diagonal = True, rtol = 1e-4, split=[50,50]):
+def inverse_cov_matrix_survey(d, z_bins, pixel_size=4, small_off_diagonal=True, rtol=1e-4, split=[50, 50]):
+    """
+    Compute the inverse covariance matrix for a survey.
+
+    Parameters:
+    - d (array-like): The distance bins.
+    - z_bins (array-like): The redshift bins.
+    - pixel_size (float, optional): The pixel size. Default is 4.
+    - small_off_diagonal (bool, optional): Whether to remove small off-diagonal terms. Default is True.
+    - rtol (float, optional): The relative difference threshold for removing small off-diagonal terms. Default is 1e-4.
+    - split (list, optional): The population split percentages. Default is [50, 50].
+
+    Returns:
+    - InvCovMatrix (list of arrays): The inverse covariance matrices for each redshift bin.
+    """
     
     d_min = d[0]
     d_max = d[-1]
@@ -114,15 +156,30 @@ def inverse_cov_matrix_survey(d, z_bins, pixel_size = 4, small_off_diagonal = Tr
     
     return InvCovMatrix
 
-def inverse_cov_matrix_joint(d, z_bins, pixel_size = 4, small_off_diagonal = True, rtol = 1e-4, splits=[50,30], contributions='all'):
-    
+def inverse_cov_matrix_joint(d, z_bins, pixel_size=4, small_off_diagonal=True, rtol=1e-4, splits=[50, 30], contributions='all'):
+    """
+    Compute the inverse covariance matrix for a joint analysis of two population splits.
+
+    Parameters:
+    - d (array-like): The distance bins.
+    - z_bins (array-like): The redshift bins.
+    - pixel_size (float, optional): The pixel size. Default is 4.
+    - small_off_diagonal (bool, optional): Whether to remove small off-diagonal terms. Default is True.
+    - rtol (float, optional): The relative difference threshold for removing small off-diagonal terms. Default is 1e-4.
+    - splits (list, optional): The bright population split percentages. Default is [50, 30].
+    - contributions (str, optional): The contributions to include in the covariance matrix. Default is 'all'.
+
+    Returns:
+    - InvCovMatrix (list of arrays): The inverse covariance matrices for each redshift bin.
+    """
+
     d_min = d[0]
     d_max = d[-1]
-    
-    print('\n ... Loading Covariance Matrices (Joint-splittings Analysis) ['+str(contributions)+'] [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
-    
-    CovMatrix = [np.loadtxt('Covariance/octupole/CovarianceMatrixOCT_'+str(contributions)+'_'+str(splits[0])+'x'+str(splits[1])+'_at_z'+str(z)+'_d'+str(int(d_min))+'to160.txt') for z in z_bins]
-            
+
+    print('\n ... Loading Covariance Matrices (Joint-splittings Analysis) [' + str(contributions) + '] [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
+
+    CovMatrix = [np.loadtxt('Covariance/octupole/CovarianceMatrixOCT_' + str(contributions) + '_' + str(splits[0]) + 'x' + str(splits[1]) + '_at_z' + str(z) + '_d' + str(int(d_min)) + 'to160.txt') for z in z_bins]
+
     if small_off_diagonal == False:
         print('\n ... Removing small off-diagonal terms vs diagonal terms ... \n')
         print(f'\n Relative difference = {rtol} \n')
@@ -131,27 +188,40 @@ def inverse_cov_matrix_joint(d, z_bins, pixel_size = 4, small_off_diagonal = Tru
             r, c = np.shape(Matrix)
             for i in np.arange(r):
                 for j in np.arange(c):
-                    value = Matrix[i,j]/np.sqrt(Matrix[i,i] * Matrix[j,j])
+                    value = Matrix[i, j] / np.sqrt(Matrix[i, i] * Matrix[j, j])
                     if value <= rtol:
-                        Matrix[i,j] = 0.
-                    else: 
-                        Matrix[i,j] = Matrix[i,j]
+                        Matrix[i, j] = 0.
+                    else:
+                        Matrix[i, j] = Matrix[i, j]
             CovMatrix[n] = Matrix
-            
+
     print('\n ... Computing the Inverse [MONOBB, MONOBF, MONOFF, DIPBF, QUADBB, QUADBF, QUADFF, HEXAT, OCTBF] ... \n')
-            
-    InvCovMatrix = [np.linalg.pinv(CovMatrix[i]) for i,_ in enumerate(z_bins)]
-    
+
+    InvCovMatrix = [np.linalg.pinv(CovMatrix[i]) for i, _ in enumerate(z_bins)]
+
     print('\n ... Success! ... \n')
-    
+
     return InvCovMatrix
         
-def fisher_matrix(d_signal, inv_cov, z_bins, bins = False):
-    
+def fisher_matrix(d_signal, inv_cov, z_bins, bins=False):
+    """
+    Compute the Fisher matrix for a given signal and inverse covariance matrix.
+
+    Parameters:
+    - d_signal (array-like): The signal as a function of redshift and distance.
+    - inv_cov (list of arrays): The inverse covariance matrices for each redshift bin.
+    - z_bins (array-like): The redshift bins.
+    - bins (bool, optional): Whether to return the Fisher matrix for each redshift bin. Default is False.
+
+    Returns:
+    - FisherMatrix (array): The Fisher matrix.
+    - FisherMatrixz (array, optional): The Fisher matrix for each redshift bin if bins=True.
+    """
+
     print('\n ... Computing the Fisher Matrix ... \n')
     print('Signal & Cov must be given as functions (z,d) and (z,d,d\') respectively. \n')
-    
-    fisher_dim= len(d_signal)
+
+    fisher_dim = len(d_signal)
     n_zs = len(z_bins)
 
     FisherMatrixz = np.empty((n_zs, fisher_dim, fisher_dim))
@@ -159,19 +229,31 @@ def fisher_matrix(d_signal, inv_cov, z_bins, bins = False):
     for i in np.arange(fisher_dim):
         for j in np.arange(fisher_dim):
             for k in np.arange(len(z_bins)):
-                FisherMatrixz[k,i,j] = d_signal[i,k] @ inv_cov[k] @ d_signal[j,k]
-                
+                FisherMatrixz[k, i, j] = d_signal[i, k] @ inv_cov[k] @ d_signal[j, k]
+
     FisherMatrix = np.sum(FisherMatrixz, axis=0)
-    
+
     print('\n ... Success! ... \n')
-    
+
     if bins == False:
         return FisherMatrix
     else:
         return FisherMatrixz
     
 def signal_to_noise(signal, cov, verbose=1):
-    
+    """
+    Compute the signal-to-noise ratio (S/N) for a given signal and covariance matrix.
+
+    Parameters:
+    - signal (array-like): The signal as a function of redshift and distance.
+    - cov (list of arrays): The covariance matrices for each redshift bin.
+    - verbose (int, optional): Whether to print the individual SNRs and cumulative SNR. Default is 1.
+
+    Returns:
+    - snr (float): The cumulative signal-to-noise ratio.
+    - individual_snrs (array): The individual signal-to-noise ratios for each redshift bin.
+    """
+
     print('\n ... Computing the S/N ... \n')
     print('Signal & Cov must be given as functions (z,d) and (z,d,d\') respectively. \n')
     print('-'*50)
